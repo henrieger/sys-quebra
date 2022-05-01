f12e3c52b53271bb369348dcf3390325a8973629
eduardo <eduardosch26@gmail.com>
2022-05-01 17:23:35 -0300

    Removendo versao2011 e adicionando funcoes de Disciplina

--- src/main/java/com/paradigmas/Models/Aluno.java
+++ src/main/java/com/paradigmas/Models/Aluno.java
@@ -5,14 +5,17 @@ import java.util.List;
 public class Aluno {
     private String grr;
     private String nome;
-    private Disciplina.Versao versao;
     private List<Matricula> matricula;
+    
+    public List<Matricula> getMatricula()
+    {
+    	return this.matricula;
+    }
 
-    public Aluno(String grr, String nome, Disciplina.Versao versao, List<Matricula> matricula)
+    public Aluno(String grr, String nome, List<Matricula> matricula)
     {
         this.grr = grr;
         this.nome = nome;
-        this.versao = versao;
         this.matricula = matricula;
     }
 }
--- src/main/java/com/paradigmas/Models/Disciplina.java
+++ src/main/java/com/paradigmas/Models/Disciplina.java
@@ -12,32 +12,37 @@ public class Disciplina {
         OPTATIVA
     }
 
-    public static enum Versao {
-        v2011,
-        v2019
-    }
-
     private String cod_disciplina;
     private String nome;
     private int periodo;
-    private Versao versao;
     private int ch_total;
     private Tipo tipo;
     private Situacao situacao;
 
 
-    public Disciplina(String cod_disciplina, String nome, int periodo, Versao versao, int ch_total, Tipo tipo, Situacao situacao)
+    public Disciplina(String cod_disciplina, String nome, int periodo, int ch_total, Tipo tipo, Situacao situacao)
     {
         this.cod_disciplina = cod_disciplina;
         this.nome = nome;
         this.periodo = periodo;
-        this.versao = versao;
         this.ch_total = ch_total;
         this.tipo = tipo;
         this.situacao = situacao;
     }
 
-    public String toString() { 
-        return "Name: '" + this.nome + "', cod: '" + this.cod_disciplina + "', versao: '" + this.versao + "'";
-    } 
+
+	public String getCod_disciplina() {
+		return this.cod_disciplina;
+	}
+
+
+	public int getPeriodo() {
+		return periodo;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
 }
